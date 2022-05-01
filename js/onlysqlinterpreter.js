bb53b9d98de97b3f52ec0cc6443796508587a6bb
ClereLearning <2022057@student.cct.ie>
2022-05-01 15:42:37 +0000

    cleaning output message div

--- js/onlysqlinterpreter.js
+++ js/onlysqlinterpreter.js
@@ -1,5 +1,20 @@
+/*window.addEventListener('load', (event) => {
+    let spinner = document.getElementById("spinnersql");
+    spinner.style.visibility = 'visible'; 
+
+  });
+  
+  window.onload = (event) => {
+    let spinner = document.getElementById("spinnersql");
+    spinner.style.visibility = 'hidden'; 
+  };
+*/
+
 function ExcecuteSQL(objhtml)
-{    
+{   
+    document.getElementById("output").innerHTML= "See results here";
+    document.getElementById("error").innerHTML= "";    
     var sqlcommand = objhtml.getAttribute('data-sql');
-    editor.setValue(sqlcommand);      
+    editor.setValue(sqlcommand);    
 }
+
